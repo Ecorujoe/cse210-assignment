5ef2f335e7036931c911839c3a2ef0601f275cbf
ecorujoe <osi24005@byui.edu>
2025-01-19 20:28:13 +0100

    modified exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Reflection.Emit;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+
+        // using the letter g as user grade
+        int g = int.Parse(userGrade);
+        string letter = "";
+
+        if (g >= 90)
+        {
+            letter = "A";
+            
+        }
+        else if (g >= 80)
+        {
+            letter = "B";
+        
+        }
+        else if (g >= 70)
+        {
+            letter = "C";
+        }
+        else if (g >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        if (g > 70)
+        {
+            Console.WriteLine("Congratulations you passed ");
+        }
+        else
+        {
+            Console.WriteLine("Faliure is a stepping stone to success. Try again next time.");
+        }
     }
 }
