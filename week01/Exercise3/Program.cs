60519ebe48f374865a47d3b8c5a77217a36b8699
ecorujoe <osi24005@byui.edu>
2025-01-20 01:24:00 +0100

    modified Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,34 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {    
+        //Console.Write("What is the magic number? ");
+        // userNumber = Console.ReadLine(); 
+        //int magicNumber = int.Parse(userNumber);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+        
+        
+        int userGuess = -1;
+        while (number != userGuess)
+        {
+            Console.Write("What is Your guess? ");
+            string guess = Console.ReadLine();
+            userGuess = int.Parse(guess);
+            
+            if (number > userGuess)
+            {
+                Console.WriteLine("Higer");
+            }
+            else if (number < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You have guessed it ");
+            }
+        }
     }
 }
